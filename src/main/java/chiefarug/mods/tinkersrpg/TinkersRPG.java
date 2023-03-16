7c036212c98ad816ef6c15830fd268c9a8fc7fde
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2023-03-16 13:13:54 +1300

    🎉Initial Commit

---
+++ src/main/java/chiefarug/mods/tinkersrpg/TinkersRPG.java
@@ -0,0 +1,16 @@
+package chiefarug.mods.tinkersrpg;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("tinkers_rpg")
+public class TinkersRPG {
+
+	@SuppressWarnings("unused")
+    private static final Logger LGGR = LogUtils.getLogger();
+
+	public TinkersRPG() {
+	}
+}
